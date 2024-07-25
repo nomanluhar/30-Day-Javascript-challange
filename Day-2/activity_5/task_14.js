a52f20907198a6a5507e7ffc0fd8655868fb04bb
nomanluhar <luharnoman409@gmail.com>
2024-07-25 23:22:10 +0530

    Day 2 task solution added

---
+++ Day-2/activity_5/task_14.js
@@ -0,0 +1,11 @@
+//Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+const operand1 = 20;
+
+const operand2 = -20;
+
+const result1 = operand1 > 0 ? "Positive" : "Negative";
+const result2 = operand2 > 0 ? "Positive" : "Negative";
+
+console.log("Result after using ternary operator for 20 is: " + result1);
+console.log("Result after using ternary operator for -20 is: " + result2);
